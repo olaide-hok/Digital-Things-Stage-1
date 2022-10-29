2f2f41338432eda91ebf803a135e22513088c159
olaide-hok <olaidekareem43@yahoo.com>
2022-10-29 21:05:23 +0100

    Complete Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
-import RiShareForwardLine from 'react-icons/ri';
 import ProfileImg from '../assets/profile__img.svg'
+import ShareIcon from '../assets/share_icon.svg'
+import ShareIconMobile from '../assets/share_mobile.svg'
 
 
 const Profile = () => {
@@ -11,7 +12,9 @@ const Profile = () => {
             <img src={ProfileImg} alt='avatar' />
             <p>Annette Black</p>
         </div>
-        {/* <RiShareForwardLine/> */}
+        <img src={ShareIcon} alt="share icon" className='share' />
+        <img src={ShareIconMobile} alt="share icon" className='shareMobile' />
+
     </ProfileWrapper>
   )
 }
@@ -20,5 +23,8 @@ const ProfileWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    font-weight: 700;
+    line-height: 30px;
+    margin-bottom: 56px;
 `
 export default Profile
