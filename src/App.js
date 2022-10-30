f43cb182358566f35f15396292c1ed926193fdcc
olaide-hok <olaidekareem43@yahoo.com>
2022-10-30 08:15:27 +0100

    Wrap social section icons in an anchor tag

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className='main'>
       <div className="section">
         <Profile/>
-        <Link link_address={'https://twitter.com'} link_name={'Twitter Link'} linkID={''}/>
+        <Link link_address={'https://twitter.com/olaide_hok'} link_name={'Twitter Link'} linkID={'twitter'}/>
         <Link link_address={'https://training.zuri.team'} link_name={'Zuri Team'} linkID={'btn__zuri'}/>
         <Link link_address={'http://books.zuri.team '} link_name={'Zuri Books'} linkID={'books'}/>
         <Link link_address={'https://books.zuri.team/python-for-beginners?ref_id=Habeeb'} link_name={'Python Books'} linkID={'book__python'}/>
@@ -18,8 +18,12 @@ function App() {
         <Link link_address={'https://books.zuri.team/design-rules'} link_name={'Design Books'} linkID={'book__design'}/>
 
         <div className='social_section'>
-          <img src={Slack} alt="slack" className='slack' />
-          <FaGithub className='github'/>
+          <a href='https://slack.com'>
+            <img src={Slack} alt="slack" className='slack' />
+          </a>
+          <a href="https://github.com/olaide-hok">
+            <FaGithub className='github'/>
+          </a>
         </div>
       </div>
       <Footer/>
