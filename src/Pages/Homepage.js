d99288729594929d6f1d41d29b062e1425325dd9
olaide-hok <olaidekareem43@yahoo.com>
2022-11-05 17:40:01 +0100

    Used react-router-dom Link to nav to contact page

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -3,6 +3,7 @@ import CustomLink from '../components/CustomLink'
 import Profile from '../components/Profile'
 import Slack from '../assets/slack.svg';
 import {FaGithub} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
@@ -14,8 +15,10 @@ const Homepage = () => {
         <CustomLink link_address={'https://books.zuri.team/python-for-beginners?ref_id=Habeeb'} link_name={'Python Books'} linkID={'book__python'}/>
         <CustomLink link_address={'https://background.zuri.team'} link_name={'Background Check for Coders'} linkID={'pitch'}/>
         <CustomLink link_address={'https://books.zuri.team/design-rules'} link_name={'Design Books'} linkID={'book__design'}/>
-        <CustomLink link_address={'/contact'} link_name={'Contact'} linkID={'contact'}/>
-
+        
+        <Link to='contact'>Contact
+        </Link>
+        {/* <CustomLink link_name={'Contact'} linkID={'contact'}/> */}
         <div className='social_section'>
           <a href='https://slack.com'>
             <img src={Slack} alt="slack" className='slack' />
